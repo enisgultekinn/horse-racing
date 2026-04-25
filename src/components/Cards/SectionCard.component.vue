b3076ede5a266c46a627e4cbc4afeb4c3e227ce7
Enis Gültekin <enisgltkn@gmail.com>
2026-04-25 17:05:26 +0300

    added section card comp

---
+++ src/components/Cards/SectionCard.component.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="section-card">
+    <span class="section-card__title">{{ title }}</span>
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  title: string
+}
+
+const { title } = defineProps<Props>()
+</script>
+
+<style scoped lang="scss">
+@use '@/assets/style/shared/mixins.scss' as *;
+.section-card {
+  border-radius: 0.5rem;
+  box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.1);
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  max-height: 100%;
+  height: 100%;
+
+  &__title {
+    font-size: var(--text-base);
+    font-weight: 600;
+    color: var(--color-neutral-900);
+  }
+}
+</style>
